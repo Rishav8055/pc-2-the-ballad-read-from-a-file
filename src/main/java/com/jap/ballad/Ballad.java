282390f0fdf9e63e78cb6703837c1df78117eaaf
rishavemail786 <rishavemail786@gmail.com>
2022-08-26 17:34:36 +0530

    SOLUTION DONE

--- src/main/java/com/jap/ballad/Ballad.java
+++ src/main/java/com/jap/ballad/Ballad.java
@@ -1,13 +1,35 @@
 package com.jap.ballad;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Ballad {
+    public static void main(String[] args) {
+        Ballad ballad = new Ballad();
+        String fileData="src/Main/resources/ballad.txt";
+        System.out.println(ballad.readPoemFromAFile("src/Main/resources/ballad.txt"));
+    }
     public String readPoemFromAFile(String fileName){
        // Write the logic to read from a file
-        return "";
+        String response="";
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(fileName);
+            int poemData;
+            while ((poemData=fileInputStream.read())!=-1){
+                char data=(char)poemData;
+                response=response+data;
+            }
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        response=response.replaceAll("\r","");
+        return response;
     }
 
 
